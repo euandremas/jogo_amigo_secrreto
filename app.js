9a10b67935c30a74e07556edb15038f24a4da3e8
Andre Santos <euandremas@gmail.com>
2025-02-10 15:56:11 -0300

    Adicionado códigos para insersão dos nomes na memória

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-let listaDeAmigos [];
+let listaDeAmigos = []; // Array para armazenar os nomes dos participantes do amigo secreto
+let amigosSecretos = []; // Acredito ser mais interessante combinar os participantes então criei essa array para armazenar os pares de amigos secretos sorteados
 
+// Função para exibir texto em elementos HTML
+function exibirTextoNaTela(tag, texto) { 
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    }
+    
+// Função para adicionar um amigo à lista de participantes
 function adicionarAmigo() { 
+
+    // Pega o nome do amigo do campo de entrada e remove espaços em branco extras que poderiam causar duplicidade caso espaços forem inseridos antes ou depois do nome. Para isso inseri o .trim()
+    let nome = document.getElementById('amigo').value.trim();
     console.log('click button');
+    
+    if (nome === "") {
+    alert('Por favor, insira um nome válido');
+    return;
+}
+listaDeAmigos.push(nome);
+console.log(`Amigo adicionado: ${nome}`);
 }
 
